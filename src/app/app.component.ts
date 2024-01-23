598ba6b2ba19b9978730139b4c2f13105676f78c
Joseph Adams <joseph.adams@blueyonder.co.uk>
2024-01-23 17:20:59 +0000

    added onclick and console log

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+  films = [
+    {
+      id: '1',
+      title: 'Joker',
+      dir: 'Todd Phillips',
+      rating: '15',
+    },
+    {
+      id: '2',
+      title: 'Wolf of Wall Street',
+      dir: 'Martin Scorsese',
+      rating: '18',
+    },
+    {
+      id: '3',
+      title: 'Ghostbusters',
+      dir: 'Ivan Reitman',
+      rating: 'PG',
+    },
+    {
+      id: '4',
+      title: 'Star Wars',
+      dir: 'George Lucs',
+      rating: 'PG',
+    },
+  ];
+
   constructor() {}
+
+  showFilm(id: string, title: string) {
+    console.log(id, title);
+  }
 }
